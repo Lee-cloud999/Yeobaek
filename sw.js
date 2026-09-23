cc938bd5cff51b375a291915f54dc3b90a5995d5
Lee-cloud999 <ggiyoo@gmail.com>
2026-09-23 14:16:24 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,31 +1,26 @@
-const CACHE='yeobaek-v2-maru-buri';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg'];
+const CACHE='yeobaek-v3-maruburi-full';
+const APP=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 
-self.addEventListener('install', event => {
+self.addEventListener('install',event=>{
   self.skipWaiting();
-  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+  event.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP)));
 });
-
-self.addEventListener('activate', event => {
+self.addEventListener('activate',event=>{
   event.waitUntil(
-    caches.keys()
-      .then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
-      .then(() => self.clients.claim())
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+    .then(()=>self.clients.claim())
   );
 });
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(cached => {
-      if (cached) return cached;
-      return fetch(event.request).then(response => {
-        if (!response || response.status !== 200) return response;
-        const copy = response.clone();
-        caches.open(CACHE).then(cache => cache.put(event.request, copy));
-        return response;
-      }).catch(() => {
-        if (event.request.mode === 'navigate') return caches.match('./index.html');
-      });
-    })
-  );
+self.addEventListener('fetch',event=>{
+  if(event.request.mode==='navigate'){
+    event.respondWith(
+      fetch(event.request).then(res=>{
+        const copy=res.clone();
+        caches.open(CACHE).then(c=>c.put('./index.html',copy));
+        return res;
+      }).catch(()=>caches.match('./index.html'))
+    );
+    return;
+  }
+  event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request)));
 });
