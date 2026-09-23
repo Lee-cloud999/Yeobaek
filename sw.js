5439d42101b9620e03e13f21a6c394964b9b18b0
Lee-cloud999 <ggiyoo@gmail.com>
2026-09-23 15:24:30 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='yeobaek-v5-3-exact-config-20260923';
-const APP=['./?v=5-3','./index.html?v=5-3','./manifest.webmanifest','./icon.svg?v=4','./icon-180.png?v=4','./icon-192.png?v=4','./icon-512.png?v=4'];
+const CACHE='yeobaek-v5-4-pull-refresh-20260923';
+const APP=['./?v=5-4','./index.html?v=5-4','./manifest.webmanifest','./icon.svg?v=4','./icon-180.png?v=4','./icon-192.png?v=4','./icon-512.png?v=4'];
 
 self.addEventListener('install',event=>{
   self.skipWaiting();
@@ -16,9 +16,9 @@ self.addEventListener('fetch',event=>{
     event.respondWith(
       fetch(event.request,{cache:'no-store'}).then(res=>{
         const copy=res.clone();
-        caches.open(CACHE).then(c=>c.put('./index.html?v=5-3',copy));
+        caches.open(CACHE).then(c=>c.put('./index.html?v=5-4',copy));
         return res;
-      }).catch(()=>caches.match('./index.html?v=5-3'))
+      }).catch(()=>caches.match('./index.html?v=5-4'))
     );
     return;
   }
